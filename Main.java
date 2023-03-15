ccf9b8fcaecd9f09b2f1220db8522f85de085ede
Bajal <mohiyudeen.bajal@nytimes.com>
2023-03-15 13:31:42 -0400

    init

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+import java.util.Properties;
+ 
+/**
+ * This program demonstrates how to make database connection with Oracle
+ * database server.
+ * @author www.codejava.net
+ *
+ */
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Connection conn1 = null;
+        Connection conn = null;
+        Connection conn3 = null;
+ 
+        try {
+            // registers Oracle JDBC driver - though this is no longer required
+            // since JDBC 4.0, but added here for backward compatibility
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String dbURL2 = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST="+System.getenv("DB_HOST")+")(PORT=1522))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ECST2)))";
+            String username = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASS");
+            long startTime = System.currentTimeMillis();
+
+            conn = DriverManager.getConnection(dbURL2, username, password);
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT ID, instrument_id,start_date FROM subscription " +
+                    "WHERE id In (1029779155,1029779163,1029779164,1029779151,1029779157,1029779159,1029779160,1029779169,1029779170)");
+
+            while (rs.next()) {
+                String lastName = rs.getString("instrument_id");
+                System.out.print(lastName +",");
+            }
+            System.out.println("Query completed in");
+            System.out.print(System.currentTimeMillis() - startTime);
+            System.out.println(" milliseconds");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+### Plain JDBC connectivity tester
+
+Compile:
+`javac Main.java`
+
+Run:
+```
+DB_HOST=myhost.company.com DB_USER=scott DB_PASS=tiger java -classpath ojdbc8-12.2.0.1.jar:. Main
+```
