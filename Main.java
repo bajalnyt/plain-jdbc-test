efd2839efefa77cd2d0894cb6891420acb5803f5
Bajal <mohiyudeen.bajal@nytimes.com>
2023-12-29 10:49:58 -0500

    update

--- Main.java
+++ Main.java
@@ -4,25 +4,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
  
-/**
- * This program demonstrates how to make database connection with Oracle
- * database server.
- * @author www.codejava.net
- *
- */
+
 public class Main {
  
     public static void main(String[] args) {
         Connection conn = null;
-        final String SQL = "SELECT ID, instrument_id,start_date FROM subscription " +
-                           "WHERE id In (1029779155,1029779163,1029779164,1029779151," +
-                           "1029779157,1029779159,1029779160,1029779169,1029779170)";
-        final int NUM_EXECUTIONS = 1000;
+        final String SQL = "select sub_acct_num from aristo_owner.subs_subscriber";
+        final int NUM_EXECUTIONS = System.getenv("NUM_EXECUTIONS")==null?10:Integer.parseInt(System.getenv("NUM_EXECUTIONS"));
 
         try {
             Class.forName("oracle.jdbc.OracleDriver");
 
-            String dbURL2 = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST="+System.getenv("DB_HOST")+")(PORT=1522))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ECST2)))";
+            //String dbURL2 = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST="+System.getenv("DB_HOST")+")(PORT=1522))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ECST2)))";
+            String dbURL2 = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST="+System.getenv("DB_HOST")+")(PORT=1522))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ARSTODV1)))";
+            
             String username = System.getenv("DB_USER");
             String password = System.getenv("DB_PASS");
             long startTime;
@@ -36,7 +31,7 @@ public class Main {
                 ResultSet rs = stmt.executeQuery(SQL);
 
                 while (rs.next()) {
-                    String instr_id = rs.getString("instrument_id");
+                    String instr_id = rs.getString("sub_acct_num");
                 }
                 durations.add(System.nanoTime() - startTime);
             }
@@ -46,6 +41,7 @@ public class Main {
             long min = Collections.min(durations);
 
             System.out.println("Min :" + min +  ", Max :" + max + ", Avg :" + sum/NUM_EXECUTIONS +" (nanoseconds)");
+            System.out.println("Min :" + min/1000000 +  ", Max :" + max/1000000 + ", Avg :" + sum/NUM_EXECUTIONS/1000000 +" (milliseconds)");
 
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
